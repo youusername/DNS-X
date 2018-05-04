7e5cbc537b0a46c6d125df448a6d8ea484ae13c1
zhang <214644496@qq.com>
2018-05-04 16:16:03 +0800

    Initial Commit

---
+++ DNS-X/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DNS-X
+//
+//  Created by zhangjing on 2018/5/4.
+//  Copyright © 2018年 214644496@qq.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DNS-X/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DNS-X
+//
+//  Created by zhangjing on 2018/5/4.
+//  Copyright © 2018年 214644496@qq.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
